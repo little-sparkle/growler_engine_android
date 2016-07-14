adb5af441737f612db473df64b8023d674af1f02
Manson Li <manson.li3307@gmail.com>
2016-07-14 15:13:24 +0800

    add files

---
+++ library/src/main/java/com/littlesparkle/growler/library/order/OrderConstants.java
@@ -0,0 +1,11 @@
+package com.littlesparkle.growler.library.order;
+
+public class OrderConstants {
+    public enum ORDER_STATUS_CODE {
+        INITIAL_STATUS, // 0
+        ORDER_ASSIGNING, // 1
+        ORDER_ASSIGNED, // 2
+        ORDER_ASSIGN_FAILED, // 3
+        ORDER_CANCELED // 4
+    }
+}
