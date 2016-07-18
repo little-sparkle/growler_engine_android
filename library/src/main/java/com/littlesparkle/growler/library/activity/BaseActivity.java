32349640414c8741ec211b3e8f32e99af0338726
Manson Li <manson.li3307@gmail.com>
2016-07-18 09:11:51 +0800

    add a function: double click to quit

--- library/src/main/java/com/littlesparkle/growler/library/activity/BaseActivity.java
+++ library/src/main/java/com/littlesparkle/growler/library/activity/BaseActivity.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.widget.Toast;
+
+import com.littlesparkle.growler.library.R;
 
 public abstract class BaseActivity extends AppCompatActivity {
-    public LayoutInflater mLayoutInflater;
-    public BaseActivity mBaseActivity;
+
+    private static final int QUIT_CHECK_INTERNAL = 2000;
+    protected boolean mIsDoubleClickToQuit = false;
+    private long mQuitTimePoint = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mLayoutInflater = this.getLayoutInflater();
-        mBaseActivity = this;
         setContentView(getLayoutResId());
         initData();
         initView();
@@ -25,16 +28,21 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void initData() {
     }
 
-    protected void startActivity(Class Activity) {
-        Intent intent = new Intent(this, Activity);
-        startActivity(intent);
-    }
-
-    protected void startService(Class service) {
-        Intent intent = new Intent(this, service);
-        startService(intent);
-    }
 
     protected abstract int getLayoutResId();
 
+    @Override
+    public void onBackPressed() {
+        if (!mIsDoubleClickToQuit) {
+            super.onBackPressed();
+        } else {
+            if ((System.currentTimeMillis() - mQuitTimePoint) > QUIT_CHECK_INTERNAL) {
+                Toast.makeText(getApplicationContext(), R.string.quit_confirmation,
+                        Toast.LENGTH_SHORT).show();
+                mQuitTimePoint = System.currentTimeMillis();
+            } else {
+                super.onBackPressed();
+            }
+        }
+    }
 }
