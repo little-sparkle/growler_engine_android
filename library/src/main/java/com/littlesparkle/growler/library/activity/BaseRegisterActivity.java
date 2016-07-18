9318d3b415e70661b1eb2a65eeae6144c3b2dae7
Manson Li <manson.li3307@gmail.com>
2016-07-18 16:53:49 +0800

    add abstract methods in base register activity

--- library/src/main/java/com/littlesparkle/growler/library/activity/BaseRegisterActivity.java
+++ library/src/main/java/com/littlesparkle/growler/library/activity/BaseRegisterActivity.java
@@ -1,10 +1,20 @@
 package com.littlesparkle.growler.library.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.View;
+import android.widget.TextView;
 
 import com.littlesparkle.growler.library.R;
 
-public class BaseRegisterActivity extends BaseActivity {
+public abstract class BaseRegisterActivity extends BaseActivity {
+    protected AppCompatEditText mMobileInput;
+    protected AppCompatEditText mAuthCodeInput;
+    protected AppCompatEditText mPwdInput;
+    protected AppCompatEditText mPwdConfInput;
+    protected AppCompatButton mRegisterBtn;
+    protected TextView mSendAuthCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +25,30 @@ public class BaseRegisterActivity extends BaseActivity {
     protected int getLayoutResId() {
         return R.layout.activity_base_register;
     }
+
+    @Override
+    protected void initView() {
+        mMobileInput = (AppCompatEditText) findViewById(R.id.input_mobile);
+        mAuthCodeInput = (AppCompatEditText) findViewById(R.id.input_auth_code);
+        mPwdInput = (AppCompatEditText) findViewById(R.id.input_password);
+        mPwdConfInput = (AppCompatEditText) findViewById(R.id.input_password_confirmation);
+        mRegisterBtn = (AppCompatButton) findViewById(R.id.register_button);
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRegisterClick();
+            }
+        });
+        mSendAuthCode = (TextView) findViewById(R.id.send_auth_code);
+        mSendAuthCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSendAuthCodeClick();
+            }
+        });
+    }
+
+    protected abstract void onSendAuthCodeClick();
+
+    protected abstract void onRegisterClick();
 }
