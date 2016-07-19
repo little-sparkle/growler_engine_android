52d2f74ee6c2ee34141ced33a6e16bc19eb19b72
Manson Li <manson.li3307@gmail.com>
2016-07-19 16:00:14 +0800

    update order status

--- library/src/main/java/com/littlesparkle/growler/library/order/OrderConstants.java
+++ library/src/main/java/com/littlesparkle/growler/library/order/OrderConstants.java
@@ -2,10 +2,17 @@ package com.littlesparkle.growler.library.order;
 
 public class OrderConstants {
     public enum ORDER_STATUS_CODE {
-        INITIAL_STATUS, // 0
-        ORDER_ASSIGNING, // 1
-        ORDER_ASSIGNED, // 2
-        ORDER_ASSIGN_FAILED, // 3
-        ORDER_CANCELED // 4
+        ORDER_ASSIGNING,            // 0
+        ORDER_ASSIGNED,             // 1
+        ORDER_CONFIRMED,            // 2
+        ORDER_READY,                // 3
+        ORDER_STARTED,              // 4
+        ORDER_FINISHED_NOT_PAYED,   // 5
+        ORDER_PAUSED,               // 6
+        ORDER_FINISHED_PAYED,       // -1
+        ORDER_INTERRUPTED,          // -2
+        ORDER_NO_RESPONSE,          // -3
+        ORDER_CANCELED_BY_DRIVER,   // -4
+        ORDER_CANCELED_BY_PASSENGER // -5
     }
 }
