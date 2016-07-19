80c56f09feb4968e7d687e5a11938f58ac82f787
Manson Li <manson.li3307@gmail.com>
2016-07-19 19:15:28 +0800

    modify driver request

--- library/src/main/java/com/littlesparkle/growler/library/user/driver/DriverRequest.java
+++ library/src/main/java/com/littlesparkle/growler/library/user/driver/DriverRequest.java
@@ -3,18 +3,32 @@ package com.littlesparkle.growler.library.user.driver;
 
 import com.littlesparkle.growler.library.http.DefaultResponse;
 import com.littlesparkle.growler.library.http.Request;
+import com.littlesparkle.growler.library.user.UserSignUpResponse;
 
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class DriverRequest extends Request<DriverRequest.DriverApi> {
 
     @Override
     protected Class<DriverApi> getServiceClass() {
         return DriverApi.class;
+    }
+
+    public Subscription signup(Subscriber<DriverSignUpResponse> subscriber,
+                               String mobile, String password, String auth_code) {
+        return mService.signup(mobile, password, auth_code)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .unsubscribeOn(Schedulers.io())
+                .subscribe(subscriber);
     }
 
     public interface DriverApi {
