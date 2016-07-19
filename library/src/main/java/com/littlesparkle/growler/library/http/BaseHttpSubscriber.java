55d4f27ec5cfb0e776b740f41bcba083c4b20a2a
Manson Li <manson.li3307@gmail.com>
2016-07-19 18:40:29 +0800

    add Progress&Toast in subscriber

--- library/src/main/java/com/littlesparkle/growler/library/http/BaseHttpSubscriber.java
+++ library/src/main/java/com/littlesparkle/growler/library/http/BaseHttpSubscriber.java
@@ -1,13 +1,36 @@
 package com.littlesparkle.growler.library.http;
 
 import com.littlesparkle.growler.library.http.api.Api;
-import com.littlesparkle.growler.library.http.api.ApiException;
-import com.littlesparkle.growler.library.log.Logger;
 
 import rx.Subscriber;
 
 public abstract class BaseHttpSubscriber<T> extends Subscriber<T> {
+    private IProgress mProgress;
+    private IToast mToast;
+
     public BaseHttpSubscriber() {
+        this(null, null);
+    }
+
+    public BaseHttpSubscriber(IProgress progress) {
+        this(progress, null);
+    }
+
+    public BaseHttpSubscriber(IToast toast) {
+        this(null, toast);
+    }
+
+    public BaseHttpSubscriber(IProgress progress, IToast toast) {
+        mProgress = progress;
+        mToast = toast;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mProgress != null) {
+            mProgress.showProgress();
+        }
     }
 
     @Override
@@ -15,16 +38,37 @@ public abstract class BaseHttpSubscriber<T> extends Subscriber<T> {
         if (isUnsubscribed()) {
             this.unsubscribe();
         }
+        if (mProgress != null) {
+            mProgress.dismissProgress();
+        }
     }
 
     @Override
     public void onError(Throwable e) {
-        Logger.e("onError: " + e.getMessage());
         if (isUnsubscribed()) {
             this.unsubscribe();
+        }
+        if (mProgress != null) {
+            mProgress.dismissProgress();
         }
         onError(Api.handleError(e));
     }
 
-    protected abstract void onError(String errorMessage);
+    protected void onError(String errorMessage) {
+        if (mToast != null) {
+            mToast.showToast(errorMessage);
+        }
+    }
+
+    public interface IProgress {
+        void showProgress();
+
+        void dismissProgress();
+    }
+
+    public interface IToast {
+        void showToast(String msg);
+
+        void dismissToast();
+    }
 }
