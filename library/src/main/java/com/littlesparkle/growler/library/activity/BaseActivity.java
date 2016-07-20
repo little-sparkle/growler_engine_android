4c59def3b9096683c767cca730c6b186e5599736
Manson Li <manson.li3307@gmail.com>
2016-07-20 16:01:11 +0800

    refactor tips in base activity

--- library/src/main/java/com/littlesparkle/growler/library/activity/BaseActivity.java
+++ library/src/main/java/com/littlesparkle/growler/library/activity/BaseActivity.java
@@ -2,11 +2,14 @@ package com.littlesparkle.growler.library.activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.littlesparkle.growler.library.R;
 import com.littlesparkle.growler.library.http.BaseHttpSubscriber;
+import com.littlesparkle.growler.library.log.Logger;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -16,6 +19,43 @@ public abstract class BaseActivity extends AppCompatActivity
     private static final int QUIT_CHECK_INTERNAL = 2000;
     protected boolean mIsDoubleClickToQuit = false;
     private long mQuitTimePoint = 0;
+
+    private static final int MSG_SHOW_PROGRESS = 1;
+    private static final int MSG_HIDE_PROGRESS = 2;
+    private static final int MSG_SHOW_TOAST = 3;
+    private static final int MSG_HIDE_TOAST = 4;
+    private Handler mTipsHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SHOW_PROGRESS:
+                    Logger.log("BaseActivity: show progress dialog");
+                    if (mProgressDialog == null) {
+                        mProgressDialog = new SweetAlertDialog(BaseActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+                        mProgressDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                        mProgressDialog.setTitleText(getString(R.string.please_waiting));
+                        mProgressDialog.setCancelable(true);
+                    }
+                    if (!mProgressDialog.isShowing()) {
+                        mProgressDialog.show();
+                    }
+                    break;
+                case MSG_HIDE_PROGRESS:
+                    Logger.log("BaseActivity: hide progress dialog");
+                    if (mProgressDialog != null && mProgressDialog.isShowing()) {
+                        mProgressDialog.dismiss();
+                    }
+                    break;
+                case MSG_SHOW_TOAST:
+                    Toast.makeText(BaseActivity.this, msg.obj.toString(), Toast.LENGTH_SHORT).show();
+                    break;
+                case MSG_HIDE_TOAST:
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,25 +92,20 @@ public abstract class BaseActivity extends AppCompatActivity
 
     @Override
     public void showProgress() {
-        if (mProgressDialog == null) {
-            mProgressDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
-            mProgressDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
-            mProgressDialog.setTitleText(getString(R.string.please_waiting));
-            mProgressDialog.setCancelable(true);
-        }
-        mProgressDialog.show();
+        mTipsHandler.removeMessages(MSG_SHOW_PROGRESS);
+        mTipsHandler.sendEmptyMessage(MSG_SHOW_PROGRESS);
     }
 
     @Override
     public void dismissProgress() {
-        if (mProgressDialog != null) {
-            mProgressDialog.hide();
-        }
+        mTipsHandler.removeMessages(MSG_HIDE_PROGRESS);
+        mTipsHandler.sendEmptyMessage(MSG_HIDE_PROGRESS);
     }
 
     @Override
     public void showToast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        mTipsHandler.removeMessages(MSG_SHOW_TOAST);
+        mTipsHandler.sendMessage(mTipsHandler.obtainMessage(MSG_SHOW_TOAST, msg));
     }
 
     @Override
